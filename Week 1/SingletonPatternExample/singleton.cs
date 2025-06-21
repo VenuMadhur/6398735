4a9d22525678071f168888f1b6f25a4c2701e691
VenuMadhur <cholletivenumadhur@gmail.com>
2025-06-21 20:54:39 +0530

    Update singleton.cs

--- Week 1/SingletonPatternExample/singleton.cs
+++ Week 1/SingletonPatternExample/singleton.cs
@@ -1,21 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+
 using System;
 
 public class Logger
 {
-    // Step 1: Private static variable that holds the single instance of Logger
     private static Logger _instance;
 
-    // Step 2: Lock for thread-safety
     private static readonly object _lock = new object();
 
-    // Step 3: Private constructor to prevent instantiation from outside the class
     private Logger() { }
 
-    // Step 4: Public method to get the single instance of Logger
     public static Logger GetInstance()
     {
-        // Double-checked locking to ensure only one instance is created
         if (_instance == null)
         {
             lock (_lock)
@@ -30,7 +25,6 @@ public class Logger
         return _instance;
     }
 
-    // Method to log messages
     public void Log(string message)
     {
         Console.WriteLine($"LOG: {message}");
@@ -41,17 +35,12 @@ class Singleton
 {
     static void Main(string[] args)
     {
-        // Test the Singleton pattern
-
-        // Get the first instance of Logger
         Logger logger1 = Logger.GetInstance();
         logger1.Log("This is the first log message.");
 
-        // Get the second instance of Logger
         Logger logger2 = Logger.GetInstance();
         logger2.Log("This is the second log message.");
 
-        // Check if both instances are the same
         Console.WriteLine("Are both instances the same? " + (logger1 == logger2));
     }
 }
