b680718ceb12e6852aef456c6efa92281778807d
VenuMadhur <cholletivenumadhur@gmail.com>
2025-07-07 20:09:38 +0530

    Delete Week 3 directory

--- Week 3/Lab5/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-namespace Lab5Inventory.Models;
-
-public class Category
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public List<Product> Products { get; set; } = new();
-}
